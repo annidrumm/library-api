69aea4b90b085c4680aae728c778e322db20f407
anni <ann-kristin.drumm@hausgold.de>
2023-01-09 17:21:02 +0100

    set up books and authors

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,39 @@
 import express, { Express, Request, Response } from 'express';
 
 const app: Express = express();
-const port: number = 401;
+const port: number = 4001;
 
-app.get('/', (req: Request, res: Response) => {
-    res.send('Hey little busy bee');
+const books = [
+    {
+        id: 1,
+        title: 'The Awakening',
+        author: 1,
+    },
+    {
+        id: 2,
+        title: 'City of Glass',
+        author: 2,
+    },
+];
+const authors = [
+    {
+        id: 1,
+        name: 'Kate Chopin',
+        books: [1],
+    },
+    {
+        id: 2,
+        name: 'Paul Auster',
+        books: [2],
+    },
+];
+
+app.get('/books', (req: Request, res: Response) => {
+    res.json(books)
+});
+
+app.get('/authors', (req: Request, res: Response) => {
+    res.json(authors)
 });
 
 app.listen(port, () => {
